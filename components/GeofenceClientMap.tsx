2b3c96355526d5ca2764d7a96bedd6327addfa2c
yasirukularathne <yasirukularathne1234@gmail.com>
2025-09-23 17:41:29 +0530

    Fix runtime error by replacing React icon with SVG for user's location marker in Leaflet map

--- components/GeofenceClientMap.tsx
+++ components/GeofenceClientMap.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import * as leaflet from "leaflet";
 import { MapContainer, TileLayer, Polygon, Popup, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Container from "@mui/material/Container";
@@ -93,7 +94,17 @@ const GeofenceClientMap: React.FC = () => {
               ))}
               {/* Show user's location */}
               {userLocation && (
-                <Marker position={userLocation}>
+                <Marker
+                  position={userLocation}
+                  icon={leaflet.divIcon({
+                    className: "custom-user-icon",
+                    html: `<div style='display: flex; align-items: center; justify-content: center;'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24' fill='#1976d2'>
+                          <path d='M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z'/>
+                        </svg>
+                      </div>`,
+                  })}
+                >
                   <Popup>
                     <Typography variant="subtitle1" fontWeight={700}>
                       Your Location
